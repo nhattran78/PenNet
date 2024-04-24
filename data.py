d2fb3f0670aabfda99662328bd75e676526b33e5
Trần Minh Nhật <88301830+nhattran78@users.noreply.github.com>
2024-04-24 22:31:50 +0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,32 @@
+#wifi information
+wifi_channel= "11"
+wifi_bssid= "C8:3A:35:46:5F:30"
+wifi_essid= " Home Company"
+wifi_encrypt= " WPA2 WPA"
+wifi_password= "12345678"
+wifi_version = "v"
+wifi_ip = "192.168.10.1"
+
+#router information
+router= "7200 Software (C7200-ADVENTERPRISEK9-M)"
+router_ip= "192.168.1.1"
+router_version = "12.4(24)TS"
+router_vulnerability= "Default Credentials Attack"
+router_username= "admin"
+router_usrpw= "admin"
+
+
+#Scan subnets
+host_up= ["192.168.0.1", "192.168.1.1", "192.168.10.1", "192.168.2.1", "192.168.3.1"]
+subnets_input= ["192.168.0.0/16"]
+
+#Scan service
+ip_scan_service= ["192.168.1.1"]
+nmap_result= ["21/tcp - state:closed - service:ftp", "22/tcp - state:closed - service:ssh", "23/tcp - state:open - service:telnet Cisco router telnetd", "25/tcp - state:closed - service:smtp", "80/tcp - state:open - service:http Cisco IOS http config", "443/tcp - state:closed - service:https"]
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
+router_vulnerability= "Default Credentials Attack"
